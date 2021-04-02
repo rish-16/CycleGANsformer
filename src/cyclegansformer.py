a87cbeb972f66ae87fa8111c2b389fbaf92926eb
rish-16 <mail.rishabh.anand@gmail.com>
2021-04-02 19:46:16 +0800

    Add optimizers to CGF

--- src/cyclegansformer.py
+++ src/cyclegansformer.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+from torch.optim import Adam
 
 from models import Generator, Discriminator
 
@@ -12,6 +13,9 @@ class CycleGAN(nn.Module):
     def __init__(self):
         self.gen = Generator()
         self.disc = Discriminator()
+
+        self.G_opt = Adam(self.gen.parameters, lr=2e-4, betas=(0.5, 0.9))
+        self.D_opt = Adam(self.disc.parameters, lr=2e-4, betas=(0.5, 0.9))
 
     def forward(self, noise):
         img = self.gen(noise)
