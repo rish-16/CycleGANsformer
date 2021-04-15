8a6eed8e026446c6ab5704ddbdfbf6f3cc3cae8a
rish-16 <mail.rishabh.anand@gmail.com>
2021-04-15 16:45:46 +0800

    Change training process implementation of CycleGANsformer

--- cyclegansformer/cyclegansformer.py
+++ cyclegansformer/cyclegansformer.py
@@ -1,55 +1,101 @@
-import numpy as np
-import torch
-import torch.nn.functional as F
-import torch.nn as nn
+import torch, os, sys
+from torch import nn
+from torch.utils.data import DataLoader
 from torch.optim import Adam
-import itertools
+from tqdm import tqdm
 
-from cyclegansformer.models import TGenerator, TDiscriminator
-from cyclegansformer.utils import LRDecay, ReplayBuffer, weights_init
+from cyclegansformer.utils import TGenerator, TDiscriminator
 
-CUTOUT_PROB = 0.3
-COLOR_PROB = 1.0
 LR = 2e-4
 EPOCHS = 200
 DECAY_EP = 100
 
 class CycleGANsformer(nn.Module):
     def __init__(self):
-        self.gen_X2Y = TGenerator()
-        self.gen_Y2X = TGenerator()        
-        self.disc_X = TDiscriminator()
-        self.disc_Y = TDiscriminator()
+        self.discX = TDiscriminator()
+        self.discY = TDiscriminator()
 
-        # custom initialise weights
-        self.gen_X2Y.apply(weights_init)
-        self.gen_Y2X.apply(weights_init)
-        self.disc_X.apply(weights_init)
-        self.disc_Y.apply(weights_init)
+        self.genX2Y = TGenerator()
+        self.genY2X = TGenerator()
 
-        self.cycle_loss = torch.nn.L1Loss()
-        self.identity_loss = torch.nn.L1Loss()
-        self.adversarial_loss = torch.nn.L1Loss()
+        self.opt_disc = Adam(
+            list(self.discX.parameters()) + list(self.discY.parameters()),
+            lr=LR,
+            betas=(0.5, 0.999)
+        )
 
-        self.optim_gen = Adam(itertools.chain(self.gen_X2Y.parameters(), self.gen_Y2X.parameters()), betas=(0.5, 0.999), lr=LR)
-        self.optim_disc_X = Adam(self.disc_X.parameters(), betas=(0.5, 0.999), lr=LR)
-        self.optim_disc_Y = Adam(self.disc_Y.parameters(), betas=(0.5, 0.999), lr=LR)
+        self.opt_gen = Adam(
+            list(self.genX2Y.parameters()) + list(self.genY2X.parameters()),
+            lr=LR,
+            betas=(0.5, 0.999)
+        )
 
-        self.lr_lambda = LRDecay(EPOCHS, 0, DECAY_EP)
-        self.lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(self.optim_gen, lr_lambda=self.lr_lambda)
-        self.lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(self.optim_disc_X, lr_lambda=self.lr_lambda)
-        self.lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(self.optim_disc_Y, lr_lambda=self.lr_lambda)
+        self.L1 = nn.L1Loss()
+        self.mse = nn.MSELoss()
 
-        self.G_opt = Adam(self.gen.parameters, lr=2e-4, betas=(0.5, 0.9))
-        self.D_opt = Adam(self.disc.parameters, lr=2e-4, betas=(0.5, 0.9))
+    def _train_fn(self, loader, gscaler, dscaler):
+        loop = tqdm(loader, leave=True)
 
-        self.g_losses = []
-        self.d_losses = []
-        self.gan_losses = []
-        self.cycle_losses = []
+        for idx, (x_img, y_img) in enumerate(loop):
+            with torch.cuda.amp.autocast():
+                fake_x = self.genY2X(y_img)
+                D_X_real = self.discX(x_img)
+                D_X_fake = self.discX(fake_x.detach())
+                D_X_real_loss = self.mse(D_X_real, torch.ones_like(D_X_real))
+                D_X_fake_loss = self.mse(D_X_fake, torch.zeros_like(D_X_fake))
+                D_X_loss = D_X_real_loss + D_X_fake_loss
 
-        self.fake_X = ReplayBuffer()
-        self.fake_Y = ReplayBuffer()
+                fake_y = self.genX2Y(x_img)
+                D_Y_real = self.discY(fake_y)
+                D_Y_fake = self.discYiscY(fake_y.detach())            
+                D_Y_real_loss = self.mse(D_Y_real, torch.ones_like(D_Y_real))
+                D_Y_fake_loss = self.mse(D_Y_fake, torch.zeros_like(D_Y_fake))
+                D_Y_loss = D_Y_real_loss + D_Y_fake_loss
 
-    def train(self):
-        print ("hello")
+                # adversarial loss
+                D_loss = (D_X_loss + D_Y_loss) / 2
+
+            opt_disc.zero_grad()
+            dscaler.scale(D_loss).backward(retain_graph=True)
+            dscaler.step(opt_disc)
+            dscaler.update()
+
+            with torch.cuda.amp.autocast():
+                # train generators
+                D_X_fake = discX(fake_x)
+                D_Y_fake = discY(fake_y)
+                loss_G_X = self.mse(D_X_fake, torch.ones_like(D_X_fake))
+                loss_G_Y = self.mse(D_Y_fake, torch.ones_like(D_Y_fake))
+
+                # cycle loss
+                cycle_Y = self.genX2Y(fake_x)
+                cycle_X = self.genY2X(fake_y)
+                cycle_Y_loss = self.L1(y_img, cycle_Y)
+                cycle_X_loss = self.L1(x_img, cycle_X)
+
+                # identity loss
+                id_Y = self.genX2Y(y_img)
+                id_X = self.genY2X(x_img)
+                id_Y_loss = self.L1(y_img, id_Y)
+                id_X_loss = self.L1(x_img, id_X)
+
+                # combined loss
+                LAMBDA_CYCLE = 10
+                LAMBDA_ID = 0
+                G_loss = loss_G_Y + loss_G_X + (cycle_Y_loss + cycle_X_loss)*LAMBDA_CYCLE + (id_Y_loss + id_X_loss)*LAMBDA_ID
+
+            opt_gen.zero_grad()
+            gscaler.scale(G_loss).backward(retain_graph=True)
+            gscaler.step(opt_gen)
+            gscaler.update()
+
+    def fit(self, dataset, epochs=200):
+        loader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=4, pin_memory=True)
+        G_scaler = torch.cuda.amp.GradScaler()
+        D_scaler = torch.cuda.amp.GradScaler()
+
+        for epoch in range(epochs):
+            self._train_fn(loader, G_scaler, D_scaler)
+
+    def forward(self, noise):
+        pass
--- cyclegansformer/utils.py
+++ cyclegansformer/utils.py
@@ -1,50 +1,203 @@
 import numpy as np
-import torch
+import torch, os
 import torch.nn.functional as F
-import torch.nn as nn
-from torch.optim import Adam
-import itertools
+from torch import nn, einsum
+from torch.utils.data import Dataset
+from einops import rearrange, repeat
+from einops.layers.torch import Rearrange
+from PIL import Image
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 
-class LRDecay(nn.Module):
-    def __init__(self, n_epochs, offset, decay_epoch):
+'''
+Inspired by and credit to Phil Wang's
+implementation of the Google's Vision Transformer.
+
+https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
+'''
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
         super().__init__()
-        epoch_flag = n_epochs - decay_epoch
-        assert (epoch_flag > 0), "Decay must begin before training ends"
-        self.n_epochs = n_epochs
-        self.offset = offset
-        self.decay_epochs = decay_epoch
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
 
-    def forward(self, epoch):
-        return 1.0 - max(0, epoch + self.offset - self.decay_epochs) / (self.n_epochs - self.decay_epochs)
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
 
-class ReplayBuffer:
-    def __init__(self, max_size=50):
-        assert (max_size > 0), "Empty buffer or trying to create a black hole. Be careful."
-        self.max_size = max_size
-        self.data = []
+class FeedForward(nn.Module):
+    def __init__(self, dim, hid_dim, dropout=0):
+        super().__init__()
+        self.ff = nn.Sequential(
+            nn.Linear(dim, hid_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hid_dim, dim),
+            nn.Dropout(dropout)
+        )
 
-    def push_and_pop(self, data):
-        to_return = []
-        for element in data.data:
-            element = torch.unsqueeze(element, 0)
-            if len(self.data) < self.max_size:
-                self.data.append(element)
-                to_return.append(element)
-            else:
-                if np.random.uniform(0, 1) > 0.5:
-                    i = np.random.randint(0, self.max_size - 1)
-                    to_return.append(self.data[i].clone())
-                    self.data[i] = element
-                else:
-                    to_return.append(element)
-        return torch.cat(to_return)
+    def forward(self, x):
+        return self.ff(x)
+    
+class MHAttention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
 
+        self.heads = heads
+        self.scale = dim_head ** -0.5 # "scaled" dot product
 
-# custom weights initialization called on netG and netD
-def weights_init(m):
-    classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
-        torch.nn.init.normal_(m.weight, 0.0, 0.02)
-    elif classname.find("BatchNorm") != -1:
-        torch.nn.init.normal_(m.weight, 1.0, 0.02)
-        torch.nn.init.zeros_(m.bias)
+        self.attend = nn.Softmax(dim = -1)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x):
+        b, n, _, h = *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t : rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
+
+        dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale # scaled dot product
+
+        attn = self.attend(dots)
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+
+        return self.to_out(out)
+
+class Encoder(nn.Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(
+                nn.ModuleList([
+                    PreNorm(dim, MHAttention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                    PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
+                ])
+            )
+
+    def forward(self, x):
+        for attn, ff in self.layers:
+            x = attn(x) + x
+            x = ff(x) + x
+        
+        return x
+
+'''
+Generator architecture inspired by TransGAN
+from Jiang Y. et al., 2021
+'''
+class TGenerator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_layer = nn.Linear(1024, 8*8*1024)
+        self.stage1 = nn.ModuleList([
+            Encoder(8*8*1024, 6, 8, 8*8*1024, 8*8*1024),
+            Encoder(8*8*1024, 6, 8, 8*8*1024, 8*8*1024),
+            Encoder(8*8*1024, 6, 8, 8*8*1024, 8*8*1024),
+            Encoder(8*8*1024, 6, 8, 8*8*1024, 8*8*1024),
+            Encoder(8*8*1024, 6, 8, 8*8*1024, 8*8*1024)
+        ])
+
+        self.ps1 = nn.PixelShuffle(2)
+
+        self.stage2 = nn.ModuleList([
+            Encoder(16*16*256, 6, 8, 16*16*256, 16*16*256),
+            Encoder(16*16*256, 6, 8, 16*16*256, 16*16*256),
+            Encoder(16*16*256, 6, 8, 16*16*256, 16*16*256),
+            Encoder(16*16*256, 6, 8, 16*16*256, 16*16*256)
+        ])
+
+        self.ps2 = nn.PixelShuffle(4)
+
+        self.stage3 = nn.ModuleList([
+            Encoder(16*16*256, 6, 8, 32*32*64, 32*32*64),
+            Encoder(16*16*256, 6, 8, 32*32*64, 32*32*64)
+        ])
+
+        self.out = nn.Linear(32*32*64, 32*32*3)
+
+    def forward(self, noise):
+        x = self.input_layer(noise)
+        x = self.stage1(x)
+        x = self.ps1(x)
+        x = self.stage2(x)
+        x = self.ps2(x)
+        x = self.stage3(x)
+        out = self.out(x)
+
+        return out
+
+'''
+Discriminator architecture inspired by TransGAN
+from Jiang Y. et al., 2021
+'''
+class TDiscriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.input_layer = nn.Linear(32*32*3, (8*8+1)*384)
+        self.stage1 = nn.ModuleList([
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384),
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384),
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384),
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384),
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384),
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384),
+            Encoder((8*8+1)*384, 6, 8, (8*8+1)*384, (8*8+1)*384)
+        ])
+        self.fc1 = nn.Linear((8*8+1)*384, 1*384)
+        self.out = nn.Linear(1*384, 1) # classification head
+
+    def forward(self, img):
+        x = self.input_layer(img)
+        x = self.stage1(x)
+        x = self.fc1(x)
+        pred = self.out(1)
+
+        return pred
+
+class ImageDatasetLoader(Dataset):
+    def __init__(self, root_x, root_y):
+        super().__init__()
+        self.root_x = root_x
+        self.root_y = root_y
+        self.transform = A.Compose(
+            [
+                A.Resize(width=256, height=256),
+                A.HorizontalFlip(p=0.5),
+                A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
+                ToTensorV2(),
+            ],
+            additional_targets={"image0": "image"},
+        )
+
+        self.x_imgs = os.listdir(root_x)
+        self.y_imgs = os.listdir(root_y)
+        self.ds_len = max(len(self.x_imgs), len(self.y_imgs))
+        self.x_len = len(self.x_imgs)
+        self.y_len = len(self.y_imgs)
+
+    def __len__(self):
+        return self.ds_len
+    
+    def __getitem__(self, index):
+        x_img = self.x_imgs[index % self.x_len]
+        y_img = self.y_imgs[index % self.y_len]
+
+        x_path = os.path.join(self.root_x, x_img)
+        y_path = os.path.join(self.root_y, y_img)
+
+        x_img = np.array(Image.open(x_path).convert("RGB"))
+        y_img = np.array(Image.open(y_path).convert("RGB"))
+
+        augmentations = self.transform(image=y_img, image0=x_img)
+        x_img = augmentations["image"]
+        y_img = augmentations["image0"]
+
+        return x_img, y_img        
